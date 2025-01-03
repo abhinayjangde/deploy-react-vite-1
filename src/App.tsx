606c4939a5dabd0ad84a19310d16802b4fff3459
Abhinay Jangde <abhinayjangde@gmail.com>
2025-01-03 15:24:14 +0530

    new msg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export default function App (){
     return (
         <>
         <div className="bg-blue-300 text-center text-4xl p-20">
-            Home page of this website
+            Hello Ankit kya haal hai ? 😁
         </div>
         </>
     )
