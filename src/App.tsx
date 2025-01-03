a5fc3c6e1d91413928b9a2dcd42d13dce914aa70
Abhinay Jangde <abhinayjangde@gmail.com>
2025-01-03 15:18:58 +0530

    app page is changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ export default function App (){
 
     return (
         <>
-        <div>
-            home
+        <div className="bg-blue-300 text-center text-4xl p-20">
+            Home page of this website
         </div>
         </>
     )
